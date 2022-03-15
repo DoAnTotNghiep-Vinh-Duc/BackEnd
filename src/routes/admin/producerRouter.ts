bc1785ec5850df4c6e9c5441e74e43cacc57d94a
ThanhVinh2712 <you@example.com>
2022-03-15 09:54:08 +0700

    feat: create producer

--- src/routes/admin/producerRouter.ts
+++ src/routes/admin/producerRouter.ts
@@ -5,3 +5,4 @@ export const producerRouter = express.Router();
 producerRouter.get("/producer", ProducerController.getAllProducer);
 producerRouter.get("/producer/:producer_id", ProducerController.getProducerById);
 producerRouter.put("/producer/:producer_id", ProducerController.updateProducerById);
+producerRouter.post("/producer", ProducerController.createProducer);
