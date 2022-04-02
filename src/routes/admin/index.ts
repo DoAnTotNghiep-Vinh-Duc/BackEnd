7e03c794385679fe4593cc285a0f65f4d8d89e26
ThanhVinh2712 <you@example.com>
2022-04-02 09:03:03 +0700

    remove producer

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,11 +1,9 @@
 import express, { Router } from 'express';
-import {producerRouter} from './producerRouter'
 import { supplierRouter } from './supplierRouter';
 import { typeProductRouter } from './typeProductRouter';
 import { sizeRouter } from './sizeRouter';
 import { colorRouter } from './colorRouter';
 export const AdminRoutes = express.Router();
-AdminRoutes.use('/', producerRouter);
 AdminRoutes.use('/', supplierRouter);
 AdminRoutes.use('/', typeProductRouter);
 AdminRoutes.use('/', sizeRouter);
--- src/routes/admin/producerRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { ProducerController } from '../../controllers/admin/ProducerController';
-export const producerRouter = express.Router();
-
-producerRouter.get("/producer", ProducerController.getAllProducer);
-producerRouter.get("/producer/:producer_id", ProducerController.getProducerById);
-producerRouter.put("/producer/:producer_id", ProducerController.updateProducerById);
-producerRouter.post("/producer", ProducerController.createProducer);
