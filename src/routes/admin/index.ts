4cde2f8277a57cb358965a285aa9845b3351a0c8
LeNguyenThanhVinh <78582885+LeNguyenThanhVinh@users.noreply.github.com>
2022-05-09 22:43:26 +0700

    fix: split admin route for order

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -3,8 +3,10 @@ import { supplierRoutes } from './supplier.routes';
 import { typeProductRoutes } from './type-product.routes';
 import { colorRoutes } from './color.routes';
 import {productRoutes} from './product.routes';
+import {orderRoutes} from './order.routes'
 export const adminRoutes = express.Router();
 adminRoutes.use('/', supplierRoutes);
 adminRoutes.use('/', typeProductRoutes);
 adminRoutes.use('/', colorRoutes);
 adminRoutes.use('/product', productRoutes);
+adminRoutes.use('/order',orderRoutes);
