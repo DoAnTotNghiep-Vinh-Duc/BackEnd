9148f04679844a175c6f0c23348ee0d63cf9d1d8
LeNguyenThanhVinh <78582885+LeNguyenThanhVinh@users.noreply.github.com>
2022-05-21 22:31:37 +0700

    fix: getAllRoomAdmin

--- src/routes/admin/room.routes.ts
+++ src/routes/admin/room.routes.ts
@@ -4,4 +4,4 @@ import { RoomController } from '../../controllers/admin/room.controller';
 import { CheckAdminMiddleware } from '../../middleware/check-admin-middleware';
 export const roomRoutes = express.Router();
 
-roomRoutes.get("/",AuthMiddleware.verifyAccessToken,CheckAdminMiddleware.isAdmin, RoomController.getAllRoomAdmin);
+roomRoutes.get("/", RoomController.getAllRoomAdmin);
--- src/services/admin/room.service.ts
+++ src/services/admin/room.service.ts
@@ -1,10 +1,20 @@
 import { ObjectId } from "mongodb";
 import { Room } from "../../models/room";
+import { Message } from "../../models/message";
 export class RoomService {
 
     static async getAllRoomAdmin(){
         try {
-            const rooms = await Room.find();
+            const rooms = await Room.aggregate([{$lookup:{from:"Account", localField:"user",foreignField:"_id", as:"user"}},{$unwind:"$user"}]);
+            let roomAndLastestMessage: Array<any>=[];
+            for (let index = 0; index < rooms.length; index++) {
+                let lastestMessage = await Message.find({room:rooms[index]._id}).sort({createdAt:-1}).limit(1)
+                const tmp = rooms[index];
+                tmp.lastestMessage = lastestMessage[0];
+                roomAndLastestMessage.push(tmp);
+                
+              }
+            console.log(roomAndLastestMessage);
             return {status:200, message:"get Room chat success", data:rooms};
           } catch (error) {
             return{status: 500,message: "Something went wrong !", error: error};
