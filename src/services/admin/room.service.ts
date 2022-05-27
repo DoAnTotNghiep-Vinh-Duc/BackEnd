ce760e121350264f4a21a9b54daff6f06d308f8f
LeNguyenThanhVinh <78582885+LeNguyenThanhVinh@users.noreply.github.com>
2022-05-27 23:41:06 +0700

    fix: getAllRoomAdmin

--- src/services/admin/room.service.ts
+++ src/services/admin/room.service.ts
@@ -17,19 +17,21 @@ export class RoomService {
             let roomHaveNoMessage:Array<any>=[];
             for (let index = 0; index < rooms.length; index++) {
               let lastestMessage = await Message.find({room:rooms[index]._id}).sort({createdAt:-1}).limit(1)
-              if(!lastestMessage){
+              console.log(lastestMessage);
+              
+              if(lastestMessage.length<=0){
                 roomHaveNoMessage.push(rooms[index]);
               }
               else{
                 const tmp = rooms[index];
                 tmp.lastestMessage = lastestMessage[0];
-                roomHaveNoMessage.push(tmp);
+                roomHaveMessage.push(tmp);
               }
             }
             console.log("roomHaveMessage",roomHaveMessage);
             roomHaveMessage.sort((a,b) => (a.lastestMessage.createdAt.getTime() <  b.lastestMessage.createdAt.getTime()) ? 1 : (( b.lastestMessage.createdAt.getTime()< a.lastestMessage.createdAt.getTime()) ? -1 : 0))
             roomAndLastestMessage = [...roomHaveMessage, ...roomHaveNoMessage]               
-            // await RedisCache.setCache(key,JSON.stringify(roomAndLastestMessage),60*5)
+            await RedisCache.setCache(key,JSON.stringify(roomAndLastestMessage),60*5)
             return {status:200, message:"get Room chat success", data:roomAndLastestMessage};
           } catch (error) {
             console.log(error);
